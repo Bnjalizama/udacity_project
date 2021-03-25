695a4e8d5592e3e6f8f8c709fc4da5f2c93845d5
benjamin lizama <blizamaleon@gmail.com>
2021-03-25 09:32:08 -0300

    docs: Update comments row 9

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
 
-# User enters the city that wants to view the data.
+# User the city that wants to view the data.
 def get_city():
     """
     Asks user to specify a city, month, and day to analyze.
