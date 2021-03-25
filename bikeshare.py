25f7058b8c7b57a7a907dad0ae2b359fe0f1d28a
benjamin lizama <blizamaleon@gmail.com>
2021-03-25 09:33:32 -0300

    docs: Update comments row 133

--- bikeshare.py
+++ bikeshare.py
@@ -130,7 +130,7 @@ def popular_combo_station(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 ###
-# Function to calculate the total time of a trip
+# Function to calculate = total time of a trip
 def travel_time(df):
     print('\nCalculating The Total and Mean Travel Time...\n')
     start_time = time.time()
